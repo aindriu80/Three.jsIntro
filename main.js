b2c1030db37b859ffee7a7de713110e826738e55
aindriu80 <aindriu80@gmail.com>
2019-06-12 15:56:11 +0100

    Completing the basics

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-const camera = new THREE.PerspectiveCamera(
-    75, 
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-    );
+function init(){
+    
+    scene = new THREE.Scene();
+    
+    camera = new THREE.PerspectiveCamera(
+        75, 
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+        );
+    
+    renderer = new THREE.WebGLRenderer({ antialias: true});
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    document.body.appendChild(renderer.domElement);
+    
+    geometry = new THREE.BoxGeometry( 2,2,2 );
+    // const material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
 
-const renderer = new THREE.WebGLRenderer();
+    const texture = new THREE.TextureLoader().load('textures/crate.gif');
+    const material = new THREE.MeshBasicMaterial( {map:texture} );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    camera.position.z = 5;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry( 2,2,2 );
-const material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
+}
 
 
 function animate(){
@@ -30,4 +39,12 @@ function animate(){
     renderer.render(scene, camera);
 }
 
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);    
+}
+
+window.addEventListener('resize', onWindowResize, false);
+init();
 animate();
